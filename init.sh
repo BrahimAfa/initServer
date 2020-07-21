84333d4d61a93147592a15302819b23bd9be2737
Brahim AFASSY <brahimafassy@gmail.com>
2020-07-21 16:06:19 +0100

    adding echo commnts

--- init.sh
+++ init.sh
@@ -3,18 +3,22 @@ set -x
 sudo apt update
 set +x
 
+echo 'installing certificats'
 set -x
 sudo apt install apt-transport-https ca-certificates curl software-properties-common
 set +x
 
+echo 'installing certificats'
 set -x
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 set +x
 
+echo 'adding original docker repos'
 set -x
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
 set +x
 
+echo 'installing docker from original repo'
 set -x
 sudo apt install docker-ce
 set +x
