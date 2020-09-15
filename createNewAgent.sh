4d5b4bdd42a1f13b77756dd0e584dbb47acd0d46
Brahim Afassy <brahimafassy@gmail.com>
2020-09-15 11:06:59 +0100

    init

--- createNewAgent.sh
+++ createNewAgent.sh
@@ -3,7 +3,7 @@ echo 'updating server'
  apt update
 
 echo '====>>> installing certificats'
- apt install apt-transport-https ca-certificates curl software-properties-common
+apt-get -y install apt-transport-https ca-certificates curl software-properties-common
  
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 
@@ -24,21 +24,18 @@ echo '====>>> installing docker compose'
  curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
  chmod +x /usr/local/bin/docker-compose
 echo '====>>> opening the backend and frontend ports in the agent machin'
+
     yes | ufw enable 
     ufw allow 80
 	ufw allow 3001
     ufw allow ssh
 	
 echo '====>>> update the ubuntu nodjs repository to version 12.X'
-# by default 
-curl -sL https://deb.nodesource.com/setup_12.x -o nodesource_setup.sh
+# by default ubuntu doesn't have the latest nodjs version
 
+curl -sL https://deb.nodesource.com/setup_12.x 	-o nodesource_setup.sh
 sudo bash nodesource_setup.sh
 
 sudo apt-get -y  install nodejs
 
 sudo apt-get -y install npm
-
-
-
-
